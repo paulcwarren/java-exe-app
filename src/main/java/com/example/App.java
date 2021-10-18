e69b33b6d4cc274b34e50b5a806878939e4b258c
Paul Warren <warrenpa@vmware.com>
2021-10-18 11:48:32 -0700

    initial commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,16 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws InterruptedException
+    {
+        while (true) {
+            System.out.println( "Hello World!");
+            Thread.sleep(1000);
+        }
+    }
+}
